f5cfdf2385b744327850a3c260e3309d252db503
Vagner Dutra <vagnerdutra@outlook.com>
2016-12-29 15:28:58 -0200

    upload file go

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "strings"
+    "log"
+    "strconv"
+)
+
+var x = 0
+
+func inc(){
+    x += 1
+}
+
+func tracer(w http.ResponseWriter, r *http.Request) {
+    r.ParseMultipartForm(64)
+    
+    trace := strings.TrimSpace(r.FormValue("trace"))
+    time := strings.TrimSpace(r.FormValue("name"))
+    
+    inc()
+    
+    fmt.Println(strconv.Itoa(x) + " - "+ time)
+    fmt.Println("---------------------")
+    fmt.Println(trace)
+    fmt.Println("---------------------")
+    fmt.Println("\n")
+    fmt.Println("\n")
+}
+
+func main() {
+    fmt.Println("PicPay Tracer")
+    fmt.Println("\n")
+    http.HandleFunc("/", tracer) // set router
+    err := http.ListenAndServe(":5003", nil) // set listen port
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
